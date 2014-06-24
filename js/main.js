128e855ec7a8b1d1c1f02d11e34c80c7793c07c4
Danny Taing <dantaing@gmail.com>
2014-06-23 23:17:28 -0400

    added gender filtering javascript functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
+function filterCards() {
+	var genderValue = document.filterForm.gender.value;
 
+	if (genderValue === "male") {
+		AddClassInContainer("female", ' hidden');
+		RemoveHiddenInContainer("male");
+		
+	} else if (genderValue === "female") {
+		AddClassInContainer("male", ' hidden');
+		RemoveHiddenInContainer("female");
+		
+	} else {
+		RemoveHiddenInContainer("male");
+		RemoveHiddenInContainer("female");
+	}
+}
+
+function AddClassInContainer(matchClass, content) {
+	var elems = document.getElementsByTagName('*'), i;
+	for (i in elems) {
+		if((' ' + elems[i].className + ' ').indexOf(' ' + matchClass + ' ') > -1) {
+			elems[i].className += content;
+		}
+	}
+}
+
+function RemoveHiddenInContainer(matchClass) {
+	var elems = document.getElementsByTagName('*'), i;
+	for (i in elems) {
+		if((' ' + elems[i].className + ' ').indexOf(' ' + matchClass + ' ') > -1) {
+			elems[i].className = elems[i].className.replace( /(?:^|\s)hidden(?!\S)/g , '' );
+		}
+	}
+}
